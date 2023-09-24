a4ae3f1dd7820987f20511e2696c3cf1617a5dbb
TK1213\qft-t <qft-tk-1213@tk-labo.com>
2023-09-24 21:55:15 +0900

    不具合入ってしまったので修正

--- lib/breath/controller/application_controller_helper.rb
+++ lib/breath/controller/application_controller_helper.rb
@@ -7,14 +7,14 @@ module Breath
     included do
       target_class = to_s.split("::")[-2].singularize.constantize
       target_name = target_class.to_s.underscore
-      current_target_name = "current_#{target_name}"
+      current_target = "current_#{target_name}"
 
       include ActionController::Cookies
     
       define_method :authenticate! do
         raise AuthenticationError unless cookies.key?("#{target_name}_id".to_sym)
-        raise AuthenticationError if send(current_target_name).nil?
-        raise AuthenticationError unless send(current_target_name).remember?(cookies[:remember_token])
+        raise AuthenticationError if send(current_target).nil?
+        raise AuthenticationError unless send(current_target).remember?(cookies[:remember_token])
 
         target = target_class.enabled.find_by(id: cookies.signed["#{target_name}_id".to_sym])
 
